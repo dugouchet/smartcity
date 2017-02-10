3ae2ae81b33fc94afb83b713ee4d120456e897be
=dugouchet <=dugouchet.a@gmail.com>
2017-02-10 15:03:00 +0100

    modified store entity

--- src/DataBundle/Entity/Store.php
+++ src/DataBundle/Entity/Store.php
@@ -1,25 +1,76 @@
 <?php
 
 namespace DataBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * Store
+ *
+ * @ORM\Table(name="store")
+ * @ORM\Entity(repositoryClass="DataBundle\Repository\StoreRepository")
  */
 class Store
 {
     /**
      * @var int
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
      * @var string
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\Type("string")
      */
     private $name;
 
     /**
      * @var string
+     * @ORM\Column(name="address", type="string", length=255)
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     * @Assert\Length(min=5)
      */
     private $address;
+    
+    /**  
+     * @var string
+     * @ORM\Column(name="city", type="string", length=255)
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2, max=40)
+     * */
+    private $city;
+    
+    /**  
+     * @var string
+     * @ORM\Column(name="state", type="string", length=255)
+     * @Assert\Type("string")
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2, max=40)
+     * */
+    private $state;
+    
+    /**  
+     * @var int
+     * @ORM\Column(name="zipcode", type="integer")
+     * @Assert\NotBlank()
+     *  @Assert\Range(
+     *      min = 3,
+     *      max = 10,
+     *      minMessage = "The ZipCode must be at least 3",
+     *      maxMessage = "The ZipCode cannot be bigger than 10"
+     * )
+     * @Assert\Type(
+     *     type="integer",
+     *     message="The value is not a valid."
+     * )
+     * */
+    private $zipcode;
     
     /**
      * Get id
@@ -78,6 +129,28 @@ class Store
     {
         return $this->address;
     }
+	public function getCity() {
+		return $this->city;
+	}
+	public function setCity( $city) {
+		$this->city = $city;
+		return $this;
+	}
+	public function getState() {
+		return $this->state;
+	}
+	public function setState( $state) {
+		$this->state = $state;
+		return $this;
+	}
+	public function getZipcode() {
+		return $this->zipcode;
+	}
+	public function setZipcode( $zipcode) {
+		$this->zipcode = $zipcode;
+		return $this;
+	}
+	
 	
 }
 
--- tests/DataBundle/Controller/DefaultControllerTest.php
+++ tests/DataBundle/Controller/DefaultControllerTest.php
@@ -5,15 +5,35 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 #use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use DataBundle\Entity\Store;
+use Doctrine\DBAL\Driver\PDOException;
+use Doctrine\DBAL\Exception\NotNullConstraintViolationException;
 class DefaultControllerTest extends WebTestCase
 {
-	private $dm;
+	protected $dm;
+	protected $em;
+	protected static $kernel;
+	protected static $container;
+	
+	public static function setUpBeforeClass()
+	{
+		self::$kernel = new \AppKernel('dev', true);
+		self::$kernel->boot();
+	
+		self::$container = self::$kernel->getContainer();
+	}
+	
+	public function get($serviceId)
+	{
+		return self::$kernel->getContainer()->get($serviceId);
+	}
 	
 	public function setUp(){
 		
 		    self::bootKernel();
-		    $this->container = static::$kernel->getContainer();
-		    $this->dm = static::$kernel->getContainer()->get('doctrine_mongodb')->getManager();
+		    //$this->container = static::$kernel->getContainer();
+		    $this->dm = static::$kernel->getContainer()->get('doctrine_mongodb')->getManager();   
+		    $this->em = static::$kernel->getContainer()->get('doctrine')->getManager();
 		    
 			$fixtures = array(
 					'DataBundle\DataFixtures\MongoDB\LoadTransactionData'
@@ -29,12 +49,66 @@ class DefaultControllerTest extends WebTestCase
     	->findTransaction('10')
     	;
     	*/
+    	/*
     	// get finder object first for our index
     	$finder = $this->container->get('fos_elastica.finder.app.transaction');
     	// find with exact phrase "demo elasticsearch" in title or content
     	$transaction = $finder->find("10");
     	    	
     	$this->assertCount(1, $transaction);
+    	*/    	
+    }
+    
+    public function testInputStore(){
     	
+    	$storeAdmin = new Store();
+    	$storeAdmin->setName('H&M');
+    	$storeAdmin->setAddress('2 rue lamartine');
+    	$storeAdmin->setCity('');
+    	$storeAdmin->setZipcode(4000000000);
+    	$storeAdmin->setState('');
+    	
+    	$validator = $this->get('validator');
+    	$listErrors = $validator->validate($storeAdmin);
+    	$this->assertCount(4, $listErrors);
+    	
+    	$storeAdmin->setCity('Natnes');
+    	$storeAdmin->setZipcode(4000000000);
+    	$storeAdmin->setState('');
+    	 
+    	$validator = $this->get('validator');
+    	$listErrors = $validator->validate($storeAdmin);
+    	$this->assertCount(3, $listErrors);
+    	
+    	$storeAdmin->setCity('Nantes');
+    	$storeAdmin->setZipcode(40000);
+    	$storeAdmin->setState('');
+    	 
+    	$validator = $this->get('validator');
+    	$listErrors = $validator->validate($storeAdmin);
+    	$this->assertCount(2, $listErrors);
+    	
+    	$storeAdmin->setCity('Nantes');
+    	$storeAdmin->setZipcode(40000);
+    	$storeAdmin->setState('France');
+    	
+    	$validator = $this->get('validator');
+    	$listErrors = $validator->validate($storeAdmin);
+    	$this->assertCount(1, $listErrors);//One error on translation
+    	
+    	
+    	
+    }
+    
+    protected function tearDown()
+    {
+    	/*
+    	parent::tearDown();
+    	$this->dm->close();
+    	$this->dm = null;
+    	
+    	$this->em->close();
+    	$this->em = null; // avoid memory leaks
+    	*/
     }
 }
